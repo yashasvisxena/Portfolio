67a2c7f1b6b4aa3ac8f7780751328afa76b9f06e
yashasvisxena <yashasvi1802@gmail.com>
2023-11-02 00:29:39 +0530

    maybe deploy fixed

--- src/Components/Cover.jsx
+++ src/Components/Cover.jsx
@@ -1,7 +1,16 @@
+
 import profile from "./images/profile.jpg";
+import background from "./images/back.png";
+
 function Cover() {
+  const coverStyle = {
+    backgroundImage: `url(${background})`,
+    backgroundSize: 'cover', // Set the background size
+    backdropFilter: 'blur(5px)', // Use backdropFilter for blur effect
+  };
+
   return (
-    <div className="cover flex-col h-full w-full rounded-xl bg-[url('public/back.png')] bg-cover backdrop-blur mb-5" id="about">
+    <div style={coverStyle} className="cover flex-col h-full w-full rounded-xl bg-cover mb-5" id="about">
       <div className="backdrop-blur-md flex h-full w-full p-6 rounded-xl items-center justify-around">
         <div className="flex h-full w-4/6 items-center">
           <img
@@ -11,14 +20,13 @@ function Cover() {
           />
 
           <div className="flex h-full flex-col m-2">
-            <h1 className="  text-2xl font-bold">Yashasvi Saxena</h1>
-            <h1 className=" text-lg">@yashasvisxena</h1>
+            <h1 className="text-2xl font-bold">Yashasvi Saxena</h1>
+            <h1 className="text-lg">@yashasvisxena</h1>
           </div>
         </div>
 
         <h1 className="h-full flex w-2/6 text-justify font-semibold text-lg">
-          FrontEnd Web Developer | Learning BackEnd Development | MERN Stack |
-          5-star rating in Java on HackerRank | C .
+          FrontEnd Web Developer | Learning BackEnd Development | MERN Stack | 5-star rating in Java on HackerRank | C .
         </h1>
       </div>
     </div>
