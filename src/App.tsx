1802dd20c601546c96c4d51743ca2c920d8c02db
yashasvisxena <yashasvi1802@gmail.com>
2026-01-06 13:52:12 +0530

    feat: initialize project with a comprehensive UI component library and updated development tooling and theme mode.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { ThemeProvider } from "@/components/theme-provider";
+
+function App() {
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <></>
+    </ThemeProvider>
+  );
+}
+
+export default App;
