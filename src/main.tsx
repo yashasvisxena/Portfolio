ad9de9c837b4dd6eefe4c2d31e3ad7434f4f2caf
yashasvisxena <yashasvi1802@gmail.com>
2026-01-06 17:55:45 +0530

    Add react-router and set up basic routing
    Installed react-router v7.11.0 and updated main.tsx to use RouterProvider. This sets up the foundation for client-side routing in the application.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/about",
+    element: <div>About</div>,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />
+);
